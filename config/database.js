3ac6314085d6d5ec0a90f09eefb5e2adae0db79a
rafcollier <your_email@domain.com>
2017-03-14 10:57:07 -0400

    changed logo/site name

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
+	//database: 'mongodb://localhost:27017/firstlast',
 	database: 'mongodb://heroku_hmf4ffhq:u4kmfe8p9nk3vjauoq74m8mhur@ds035593.mlab.com:35593/heroku_hmf4ffhq',
 	secret: 'thebigsecret'
 }
